bd31621641872a33ad707c01c46a027d661ece0a
Alessandro Solbiati <lessandro@fb.com>
2022-02-02 12:53:59 +0000

    boilerplate

---
+++ src/index.js
@@ -0,0 +1,23 @@
+var game = new Phaser.Game(
+	800,
+	600,
+	Phaser.CANVAS,
+	{
+		preload: preload,
+		create: create,
+		update:upate
+	});
+
+function preload(){
+
+}
+
+function create(){
+	function update(){
+
+	}
+}
+
+
+
+
